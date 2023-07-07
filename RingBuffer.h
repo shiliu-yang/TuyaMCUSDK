790223daa46dca4202f39f9434adbfc3854a2155
yangjie <shiliu.yang@tuya.com>
2023-07-07 19:05:24 +0800

    add ring buffer

--- RingBuffer.h
+++ RingBuffer.h
@@ -1,11 +1,24 @@
 #pragma once
 
+#include <Arduino.h>
+
 class RingBuffer {
 public:
 
+  RingBuffer(void);
+  RingBuffer(uint32_t size);
+  ~RingBuffer();
+
+  uint32_t available(void);
+  void write(uint8_t *data, uint32_t len);
+  uint32_t read(uint8_t *data, uint32_t len);
 
 private:
-  uint16_t _bufferSize = 0;
-  uint8_t *buffer = NULL;
+  uint8_t *_buffer = NULL;
+
+  uint32_t _bufferSize = 0;
+  uint32_t _writeIndex = 0;
+  uint32_t _readIndex = 0;
+  uint32_t _availableBytes = 0;
 };
 
